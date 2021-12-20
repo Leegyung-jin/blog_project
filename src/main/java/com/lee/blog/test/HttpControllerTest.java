b3f833f0c8d44bb1545d2c11bae8b498231ee353
LeeGyung-jin <0l055225757@gmail.com>
2021-12-20 23:18:37 +0900

    # Springboot ~ch.14

---
+++ src/main/java/com/lee/blog/test/HttpControllerTest.java
@@ -0,0 +1,40 @@
+package com.lee.blog.test;
+
+import org.springframework.web.bind.annotation.*;
+
+// 사용자가 요청 -> 응답(HTML 파일)
+// @Controller
+
+// 사용자가 요청 -> 응답(Data)
+@RestController
+public class HttpControllerTest {
+
+    // 인터넷 브라우저의 요청은 get밖에 할 수 없다. (주소창에 넣어서 하는 것)
+    // http://localhost:8080/http/get (select)
+    @GetMapping("/http/get")
+//    public String getTest(@RequestParam int id, @RequestParam String username){
+//        return "get 요청: " + id + ", " + username;
+//    }
+    public String getTest(Member m){    // id=1&username=ssar&password=1234&emai=ssar@nate.com와 같은 쿼리 스트링을 Member에 넣어줄 수 있다.
+        return "get 요청: " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+    }
+
+    // http://localhost:8080/http/post  (create)
+    @PostMapping("/http/post")  // text/plain, application/json
+    public String postTest(@RequestBody Member m){  // MessageConverter(스프링부트)
+        return "post 요청: "  + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+    }
+
+    // http://localhost:8080/http/put   (update)
+    @PutMapping("/http/put")
+    public String putTest(@RequestBody Member m){
+        return "put 요청: " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+    }
+
+    // http://localhost:8080/http/delete    (delete)
+    @DeleteMapping("/http/delete")
+    public String deleteTest(){
+        return "delete 요청";
+    }
+
+}
