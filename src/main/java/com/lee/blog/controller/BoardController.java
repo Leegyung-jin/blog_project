717ae2c31120e0919b1002b1999c2e73c74515a3
LeeGyung-jin <0l055225757@gmail.com>
2022-01-07 00:25:18 +0900

    # Springboot ch.54 - 글 목록 출력시키기

--- src/main/java/com/lee/blog/controller/BoardController.java
+++ src/main/java/com/lee/blog/controller/BoardController.java
@@ -1,18 +1,26 @@
 package com.lee.blog.controller;
 
 import com.lee.blog.config.auth.PrincipalDetail;
+import com.lee.blog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     // public String index(@AuthenticationPrincipal PrincipalDetail principal){  // 컨트롤러에서 어떻게 세션을 찾을까?
     @GetMapping({"", "/"})
-    public String index(){  // 컨트롤러에서 어떻게 세션을 찾을까?
-        // /WEB-INF/views/index.jsp로 경로를 찾아간다.
-        return "index";
+    public String index(Model model){
+        model.addAttribute("boards", boardService.글목록());
+
+        // /WEB-INF/views/index.jsp로 경로를 찾아간다. frefix, suffix
+        return "index"; // viewResolver가 작동한다.
     }
 
     // User권한이 필요하다
--- src/main/java/com/lee/blog/repository/BoardRepository.java
+++ src/main/java/com/lee/blog/repository/BoardRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
-
+    // 아무 함수도 없지만 JPARepository가 모든 것을 들고 있다.
 }
